665a975cef4dbe187dbda6ec8180dffdc70d2475
yoosername <yoosername@local>
2016-09-11 15:39:48 +0100

    added more types and some examle templates - container monitor works now

--- monitor.js
+++ monitor.js
@@ -1,7 +1,7 @@
 var monitor = require('node-docker-monitor');
 const exec = require('child_process').exec;
 var GATEWAY;
-var pluginManager = require('./lib/plugin-manager').getInstance();
+var pluginManager = require('./lib/plugins/PluginManager').getInstance();
 
 exec("netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'", (error, stdout, stderr) => {
   if (error) {
@@ -15,16 +15,25 @@ exec("netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'", (error, stdout, stde
 
 monitor({
     onContainerUp: function(container) {
+      //console.log(container);
       if( container["Ports"][0] && container["Ports"][0]["PublicPort"]){
-        pluginManager.addPlugin(container["Ports"][0]["PublicPort"]);
+        pluginManager.addPlugin(container.Id, container["Ports"][0]["PublicPort"]);
       }
     },
 
     onContainerDown: function(container) {
-        //console.log('\n\nContainer down: ', container)
+        pluginManager.removePluginByContainerId(container.Id);
     }
 });
 
 pluginManager.on("plugin:added", function(plugin){
-  console.log("New plugin detected and loaded: ", plugin.getKey());
+  console.log("Loaded Plugin from container (", plugin.getContainerId(), ") with key: ", plugin.getKey());
+})
+
+pluginManager.on("plugin:removed", function(plugin){
+  console.log("Removed plugin from container (", plugin.getContainerId(), ") with key: ", plugin.getKey());
+})
+
+pluginManager.on("module:added", function(module){
+  console.log("Loaded plugin module (", module.getName(),") for plugin: ", module.getPluginKey());
 })
--- plugins/simple-plugin/plugin.js
+++ plugins/simple-plugin/plugin.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
+// Plugin descriptor
 app.use('/plugin.json', express.static('plugin.json'));
+
+// Site decorator
+app.use('/decorator', express.static('templates/decorator.html'));
+
+// Webpage decorated by the above
+app.use('/example', express.static('templates/example.html'));
 
 app.listen(8080, function () {
   console.log('Example plugin app listening on port 8080!');
