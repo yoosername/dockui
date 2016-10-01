1f1dc8e76114fbb999d7b40d91e64987f7863ad7
yoosername <yoosername@local>
2016-10-01 20:32:58 +0100

    added form auth plugin

---
+++ bundled-plugins/form-auth-plugin/plugin.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+// TODO: Add ability to disable and enable discovered plugins
+
+// Plugin descriptor
+//app.use('/plugin.json', express.static('plugin.json'));
+app.use('/plugin.yml', express.static('plugin.yml'));
+
+// Webpage decorated by the above
+app.all('/login*', function(req, res){
+  console.log("Request type: ", req.method);
+  res.sendFile(path.join(__dirname, './templates', 'login.html'));
+});
+
+app.listen(8080, function () {
+  console.log('Form Auth plugin listening on port 8080!');
+});
