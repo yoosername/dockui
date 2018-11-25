7769871de10068576280bb1747182d3c09e4c7ba
yoosername <yoosername@local>
2018-11-25 22:16:24 +0000

    added ideas into WebService

--- src/web/WebService.js
+++ src/web/WebService.js
@@ -17,14 +17,15 @@ class WebService{
     console.log("\n\n[WebService] : Stopping");
   }
 
-  // Add middleware (like a servlet filter)
+  // Add / Remove middleware route (Run before / after Handlers)
   // - this should Build a new Router and add all middleare from 
   // - previous one plus our new one, then switch old for new
   // - possibly do switch only after no new routes modified for
   // -  well deinfed timeout latch
-  // Remove middleware
-  // Add handler
-  // Remove handler
+  // Add / Remove Req handler ( GET, POST etc )
+  // Add / Remove route (specialized middleware - translates bas routes to plugin routes (Run first))
+  // Add / Remove authentication handler (special middleware (Run before other middleware))
+  // Add / Remove authorization handler (special middleware (Run before other middleware))
 
 }
 
