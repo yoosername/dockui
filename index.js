b68747300bf548f2cb82c3e1093802ec49205075
yoosername <yoosername@local>
2019-01-16 23:16:38 +0000

    sorted the docs

--- index.js
+++ index.js
@@ -1,108 +1,109 @@
-const SecurityContext = require("./src/app/security/SecurityContext");
-const WebResourceModule = require("./src/app/module/impl/WebResourceModule");
-const RouteModule = require("./src/app/module/impl/RouteModule");
-const AuthenticationProviderModule = require("./src/app/module/impl/AuthenticationProviderModule");
-const WebhookModule = require("./src/app/module/impl/WebhookModule");
-const WebPageModule = require("./src/app/module/impl/WebPageModule");
+
 const ApiModule = require("./src/app/module/impl/ApiModule");
-const WebItemModule = require("./src/app/module/impl/WebItemModule");
-const AuthorizationProviderModule = require("./src/app/module/impl/AuthorizationProviderModule");
-const WebFragmentModule = require("./src/app/module/impl/WebFragmentModule");
-const Module = require("./src/app/module/Module");
-const DockUIApps = require("./src/app/DockUIApps");
-const AuthenticationProviderModuleLoader = require("./src/app/loader/impl/AuthenticationProviderModuleLoader");
-const WebFragmentModuleLoader = require("./src/app/loader/impl/WebFragmentModuleLoader");
-const WebPageModuleLoader = require("./src/app/loader/impl/WebPageModuleLoader");
-const WebhookModuleLoader = require("./src/app/loader/impl/WebhookModuleLoader");
-const CachableModuleLoader = require("./src/app/loader/impl/CachableModuleLoader");
-const UrlAppLoader = require("./src/app/loader/impl/UrlAppLoader");
-const AuthorizationProviderModuleLoader = require("./src/app/loader/impl/AuthorizationProviderModuleLoader");
-const RouteModuleLoader = require("./src/app/loader/impl/RouteModuleLoader");
-const WebResourceModuleLoader = require("./src/app/loader/impl/WebResourceModuleLoader");
-const ApiModuleLoader = require("./src/app/loader/impl/ApiModuleLoader");
-const DockerEventsDelegatingAppLoader = require("./src/app/loader/impl/DockerEventsDelegatingAppLoader");
-const WebItemModuleLoader = require("./src/app/loader/impl/WebItemModuleLoader");
-const AppLoader = require("./src/app/loader/AppLoader");
-const ModuleLoader = require("./src/app/loader/ModuleLoader");
-const HttpClient = require("./src/app/http/HttpClient");
-const RouteModuleDescriptor = require("./src/app/descriptor/impl/RouteModuleDescriptor");
 const ApiModuleDescriptor = require("./src/app/descriptor/impl/ApiModuleDescriptor");
-const WebResourceModuleDescriptor = require("./src/app/descriptor/impl/WebResourceModuleDescriptor");
-const WebhookModuleDescriptor = require("./src/app/descriptor/impl/WebhookModuleDescriptor");
-const AuthenticationProviderModuleDescriptor = require("./src/app/descriptor/impl/AuthenticationProviderModuleDescriptor");
-const AuthorizationProviderModuleDescriptor = require("./src/app/descriptor/impl/AuthorizationProviderModuleDescriptor");
-const WebItemModuleDescriptor = require("./src/app/descriptor/impl/WebItemModuleDescriptor");
-const WebFragmentModuleDescriptor = require("./src/app/descriptor/impl/WebFragmentModuleDescriptor");
-const WebPageModuleDescriptor = require("./src/app/descriptor/impl/WebPageModuleDescriptor");
-const AppDescriptor = require("./src/app/descriptor/AppDescriptor");
-const ModuleDescriptor = require("./src/app/descriptor/ModuleDescriptor");
-const AppService = require("./src/app/service/AppService");
-const AppPermission = require("./src/app/permission/AppPermission");
+const ApiModuleLoader = require("./src/app/loader/impl/ApiModuleLoader");
 const App = require("./src/app/App");
-const mocks = require("./src/util/mocks");
-const MockDockerClient = require("./src/util/MockDockerClient");
-const validate = require("./src/util/validate");
-const WebService = require("./src/web/WebService");
-const events = require("./src/constants/events");
-const errors = require("./src/constants/errors");
-const EventService = require("./src/events/EventService");
-const LifecycleEventsStrategy = require("./src/events/strategy/LifecycleEventsStrategy");
-const helper = require("./src/events/strategy/helper");
-const InMemoryAppStore = require("./src/store/impl/InMemoryAppStore");
+const AppDescriptor = require("./src/app/descriptor/AppDescriptor");
+const AppLoader = require("./src/app/loader/AppLoader");
+const AppPermission = require("./src/app/permission/AppPermission");
+const AppService = require("./src/app/service/AppService");
 const AppStore = require("./src/store/AppStore");
+const AuthenticationProviderModule = require("./src/app/module/impl/AuthenticationProviderModule");
+const AuthenticationProviderModuleDescriptor = require("./src/app/descriptor/impl/AuthenticationProviderModuleDescriptor");
+const AuthenticationProviderModuleLoader = require("./src/app/loader/impl/AuthenticationProviderModuleLoader");
+const AuthorizationProviderModule = require("./src/app/module/impl/AuthorizationProviderModule");
+const AuthorizationProviderModuleDescriptor = require("./src/app/descriptor/impl/AuthorizationProviderModuleDescriptor");
+const AuthorizationProviderModuleLoader = require("./src/app/loader/impl/AuthorizationProviderModuleLoader");
+const CachableModuleLoader = require("./src/app/loader/impl/CachableModuleLoader");
+const DockUIApps = require("./src/app/DockUIApps");
+const DockerEventsDelegatingAppLoader = require("./src/app/loader/impl/DockerEventsDelegatingAppLoader");
+const EventService = require("./src/events/EventService");
+const HttpClient = require("./src/app/http/HttpClient");
+const InMemoryAppStore = require("./src/store/impl/InMemoryAppStore");
+const LifecycleEventsStrategy = require("./src/events/strategy/LifecycleEventsStrategy");
+const MockDockerClient = require("./src/util/MockDockerClient");
+const Module = require("./src/app/module/Module");
+const ModuleDescriptor = require("./src/app/descriptor/ModuleDescriptor");
+const ModuleLoader = require("./src/app/loader/ModuleLoader");
+const RouteModule = require("./src/app/module/impl/RouteModule");
+const RouteModuleDescriptor = require("./src/app/descriptor/impl/RouteModuleDescriptor");
+const RouteModuleLoader = require("./src/app/loader/impl/RouteModuleLoader");
+const SecurityContext = require("./src/app/security/SecurityContext");
+const UrlAppLoader = require("./src/app/loader/impl/UrlAppLoader");
+const WebFragmentModule = require("./src/app/module/impl/WebFragmentModule");
+const WebFragmentModuleDescriptor = require("./src/app/descriptor/impl/WebFragmentModuleDescriptor");
+const WebFragmentModuleLoader = require("./src/app/loader/impl/WebFragmentModuleLoader");
+const WebItemModule = require("./src/app/module/impl/WebItemModule");
+const WebItemModuleDescriptor = require("./src/app/descriptor/impl/WebItemModuleDescriptor");
+const WebItemModuleLoader = require("./src/app/loader/impl/WebItemModuleLoader");
+const WebPageModule = require("./src/app/module/impl/WebPageModule");
+const WebPageModuleDescriptor = require("./src/app/descriptor/impl/WebPageModuleDescriptor");
+const WebPageModuleLoader = require("./src/app/loader/impl/WebPageModuleLoader");
+const WebResourceModule = require("./src/app/module/impl/WebResourceModule");
+const WebResourceModuleDescriptor = require("./src/app/descriptor/impl/WebResourceModuleDescriptor");
+const WebResourceModuleLoader = require("./src/app/loader/impl/WebResourceModuleLoader");
+const WebService = require("./src/web/WebService");
+const WebhookModule = require("./src/app/module/impl/WebhookModule");
+const WebhookModuleDescriptor = require("./src/app/descriptor/impl/WebhookModuleDescriptor");
+const WebhookModuleLoader = require("./src/app/loader/impl/WebhookModuleLoader");
+const errors = require("./src/constants/errors");
+const events = require("./src/constants/events");
+const helper = require("./src/events/strategy/helper");
+const mocks = require("./src/util/mocks");
+const validate = require("./src/util/validate");
 
 
 module.exports = {
-SecurityContext,
-WebResourceModule,
-RouteModule,
-AuthenticationProviderModule,
-WebhookModule,
-WebPageModule,
-ApiModule,
-WebItemModule,
-AuthorizationProviderModule,
-WebFragmentModule,
-Module,
+App,
 DockUIApps,
-AuthenticationProviderModuleLoader,
-WebFragmentModuleLoader,
-WebPageModuleLoader,
-WebhookModuleLoader,
-CachableModuleLoader,
-UrlAppLoader,
-AuthorizationProviderModuleLoader,
-RouteModuleLoader,
-WebResourceModuleLoader,
-ApiModuleLoader,
-DockerEventsDelegatingAppLoader,
-WebItemModuleLoader,
-AppLoader,
-ModuleLoader,
-HttpClient,
-RouteModuleDescriptor,
-ApiModuleDescriptor,
-WebResourceModuleDescriptor,
-WebhookModuleDescriptor,
-AuthenticationProviderModuleDescriptor,
-AuthorizationProviderModuleDescriptor,
-WebItemModuleDescriptor,
-WebFragmentModuleDescriptor,
-WebPageModuleDescriptor,
 AppDescriptor,
 ModuleDescriptor,
-AppService,
+ApiModuleDescriptor,
+AuthenticationProviderModuleDescriptor,
+AuthorizationProviderModuleDescriptor,
+RouteModuleDescriptor,
+WebFragmentModuleDescriptor,
+WebItemModuleDescriptor,
+WebPageModuleDescriptor,
+WebResourceModuleDescriptor,
+WebhookModuleDescriptor,
+HttpClient,
+AppLoader,
+ModuleLoader,
+ApiModuleLoader,
+AuthenticationProviderModuleLoader,
+AuthorizationProviderModuleLoader,
+CachableModuleLoader,
+DockerEventsDelegatingAppLoader,
+RouteModuleLoader,
+UrlAppLoader,
+WebFragmentModuleLoader,
+WebItemModuleLoader,
+WebPageModuleLoader,
+WebResourceModuleLoader,
+WebhookModuleLoader,
+Module,
+ApiModule,
+AuthenticationProviderModule,
+AuthorizationProviderModule,
+RouteModule,
+WebFragmentModule,
+WebItemModule,
+WebPageModule,
+WebResourceModule,
+WebhookModule,
 AppPermission,
-App,
-mocks,
-MockDockerClient,
-validate,
-WebService,
-events,
+SecurityContext,
+AppService,
 errors,
+events,
 EventService,
 LifecycleEventsStrategy,
 helper,
-InMemoryAppStore,
 AppStore,
+InMemoryAppStore,
+MockDockerClient,
+mocks,
+validate,
+WebService,
 }
--- tools/build_index.sh
+++ tools/build_index.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 JSON="\nmodule.exports = {\n"
+CONSTS=""
 
-for FILE in $(find ./src -type f | grep .js | grep -v spec)
+for FILE in $(find ./src -type f | grep .js | grep -v spec | sort)
 do
     DIR=$(dirname $FILE)
     NAME=$(basename $FILE | awk -F. '{print $1}')
     JSON="     ${JSON}${NAME},\n"
-    echo "const ${NAME} = require(\"${DIR}/$NAME\");"
+    CONSTS="$CONSTS\nconst ${NAME} = require(\"${DIR}/$NAME\");"
 done
+
+CONSTS=$(echo -e $CONSTS | sort)
+echo "${CONSTS/; /;\n}"
 
 JSON="$JSON}"
 echo
